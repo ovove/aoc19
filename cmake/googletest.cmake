92cddaf88ae905f4aa585e890603f5dbf58a6957
Ove Svensson <ove.svensson@isv41.se>
2019-11-30 08:47:40 +0100

    Fix for include path to googletest include files.

--- cmake/googletest.cmake
+++ cmake/googletest.cmake
@@ -11,17 +11,26 @@ ExternalProject_Add(
 )
 
 ExternalProject_Get_Property(googletest binary_dir)
+ExternalProject_Get_Property(googletest source_dir)
 
 add_library(gtest UNKNOWN IMPORTED)
 add_library(gtest_main UNKNOWN IMPORTED)
 set_target_properties(gtest PROPERTIES IMPORTED_LOCATION ${binary_dir}/lib/libgtest.a)
+#set_target_properties(gtest PROPERTIES INCLUDE_DIRECTORIES ${source_dir}/googletest/include)
 set_target_properties(gtest_main PROPERTIES IMPORTED_LOCATION ${binary_dir}/lib/libgtest_main.a)
+#set_target_properties(gtest_main PROPERTIES INCLUDE_DIRECTORIES ${source_dir}/googletest/include)
 add_dependencies(gtest googletest)
 add_dependencies(gtest_main googletest)
+include_directories(${source_dir}/googletest/include)
 
 add_library(gmock UNKNOWN IMPORTED)
 add_library(gmock_main UNKNOWN IMPORTED)
 set_target_properties(gmock PROPERTIES IMPORTED_LOCATION ${binary_dir}/lib/libgmock.a)
+#set_target_properties(gmock PROPERTIES INCLUDE_DIRECTORIES ${source_dir}/googlemock/include)
 set_target_properties(gmock_main PROPERTIES IMPORTED_LOCATION ${binary_dir}/lib/libgmock_main.a)
+#set_target_properties(gmock_main PROPERTIES INCLUDE_DIRECTORIES ${source_dir}/googlemock/include)
 add_dependencies(gmock googletest)
 add_dependencies(gmock_main googletest)
+include_directories(${source_dir}/googlemock/include)
+
+# @todo replace include_directories with some target propery
