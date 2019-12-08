e322ec48da104c5ee257475ff001f8d335887469
Ove Svensson <ove.svensson@isv41.se>
2019-12-08 13:42:33 +0100

    vector -> unordered_set, gives 500 time speed improvement.

--- day03/part02/puzzle.cc
+++ day03/part02/puzzle.cc
@@ -4,13 +4,11 @@
 #include <iterator>
 #include <map>
 #include <regex>
+#include <set>
 #include <string>
 #include <string_view>
+#include <unordered_set>
 #include <vector>
-
-// #include <range/v3/all.hpp>
-
-template <typename T> class TD;
 
 namespace {
 
@@ -30,38 +28,47 @@ inline std::ostream& operator<<(std::ostream& os, Point p) {
     return os << '(' << p.x << ',' << p.y << ')';
 }
 
-std::vector<Point> path_to_points(std::string_view path) {
-    std::vector<Point> result;
+} // namespace
+
+template <> struct std::hash<Point> {
+    std::size_t operator()(Point p) const {
+        const hash<int> hi;
+        return hi(p.x) ^ hi(p.y);
+    }
+};
+
+namespace {
+
+auto path_to_points(std::string_view path) {
     const std::map<std::string_view, Point> steps{{"L", {-1, 0}},
                                                   {"R", {+1, 0}},
                                                   {"U", {0, +1}},
                                                   {"D", {0, -1}}};
+    std::vector<Point> result;
     Point pos{0, 0};
     result.push_back(pos);
     std::regex reg(R"(([LRUD])([1-9][0-9]*),?)");
-    const std::cregex_iterator rbeg = std::cregex_iterator(std::begin(path), std::end(path), reg);
-    const std::cregex_iterator rend = std::cregex_iterator();
-    for (auto it = rbeg; it != rend; ++it) {
-        const auto match = *it;
-        const auto dir = match[1].str();
-        const auto step = steps.at(dir);
-        const auto dist = std::stoi(match[2].str());
-        for (int d = 0; d < dist; ++d) {
-            pos += step;
-            result.push_back(pos);
-        }
-    }
+    std::for_each(std::cregex_iterator(std::begin(path), std::end(path), reg),
+                  std::cregex_iterator(),
+                  [&steps, &pos, &result](const auto& match) {
+                      const auto dir = match[1].str();
+                      const auto step = steps.at(dir);
+                      const auto dist = std::stoi(match[2].str());
+                      for (int d = 0; d < dist; ++d) {
+                          pos += step;
+                          result.push_back(pos);
+                      }
+                  });
     return result;
 }
 
-std::vector<Point> intersections(const std::vector<Point>& path1, const std::vector<Point>& path2) {
-    std::vector<Point> result;
+auto intersections(const std::vector<Point>& path1, const std::vector<Point>& path2) {
+    const std::unordered_set<Point> path2_as_set(std::begin(path2), std::end(path2));
+    std::unordered_set<Point> result;
     for (auto p : path1) {
         if (p == Point{0, 0}) continue;
-        const auto same_point = [p1 = p](auto p2){ return p1 == p2; };
-        if (not std::any_of(std::begin(path2), std::end(path2), same_point)) continue;
-        if (std::any_of(std::begin(result), std::end(result), same_point)) continue;
-        result.push_back(p);
+        if (0 != path2_as_set.count(p)) continue;
+        result.insert(p);
     }
     return result;
 }
@@ -71,14 +78,14 @@ inline unsigned find_shortest_intersection_distance(std::string_view path1,
     const auto p1 = path_to_points(path1);
     const auto p2 = path_to_points(path2);
     const auto cross = intersections(p1, p2);
-    std::vector<unsigned> distances;
+    std::set<unsigned> distances;
     for (const auto x : cross) {
         const auto it1 = std::find(std::begin(p1), std::end(p1), x);
         const auto it2 = std::find(std::begin(p2), std::end(p2), x);
         const auto d1 = std::distance(std::begin(p1), it1);
         const auto d2 = std::distance(std::begin(p2), it2);
         const auto tot = d1 + d2;
-        distances.push_back(tot);
+        distances.insert(tot);
     }
     const auto result = std::min_element(std::begin(distances), std::end(distances));
     return *result;
